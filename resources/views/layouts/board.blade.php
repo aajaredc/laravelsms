82f8d8e7406fb891af14062cf8761e5ba3b9383b
Jared C <jaredcaast@gmail.com>
2019-08-19 19:54:00 -0400

    More authentication
    It's starting to get somewhere. The naming schema is a mess though

--- resources/views/layouts/board.blade.php
+++ resources/views/layouts/board.blade.php
@@ -36,9 +36,34 @@
               <input style="width: 100%;" type="text" class="form-control" placeholder="Search" aria-label="" aria-describedby="basic-addon1">
             </form>
             <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link" href="#">Sign-In <i class="fas fa-sign-in-alt"></i><span class="sr-only">(current)</span></a>
-              </li>
+							@guest
+									<li class="nav-item">
+											<a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+									</li>
+									@if (Route::has('register'))
+											<li class="nav-item">
+													<a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+											</li>
+									@endif
+							@else
+									<li class="nav-item dropdown">
+											<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+													{{ Auth::user()->username }} <span class="caret"></span>
+											</a>
+
+											<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+													<a class="dropdown-item" href="{{ route('logout') }}"
+														 onclick="event.preventDefault();
+																					 document.getElementById('logout-form').submit();">
+															{{ __('Logout') }}
+													</a>
+
+													<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+															@csrf
+													</form>
+											</div>
+									</li>
+							@endguest
             </ul>
           </div>
         </nav>
