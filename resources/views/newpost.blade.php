0f04f113521c0e3e9aeb9f597ea59547836a0676
Jared C <jaredcaast@gmail.com>
2019-08-20 13:00:46 -0400

    Modified tag input
    Modified the tag input for the new posts so that it better reflects the style as a whole

--- resources/views/newpost.blade.php
+++ resources/views/newpost.blade.php
@@ -26,12 +26,10 @@
                         </div>
 
                         <div class="form-group row">
-                          <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Tags') }}</label>
+                          <label for="tags" class="col-md-4 col-form-label text-md-right">{{ __('Tags') }}</label>
 
                           <div class="col-md-6">
-                            <select multiple data-role="tagsinput">
-
-                            </select>
+                            <input id="tags" class="form-control" name="tags" data-role="tagsinput"/>
                           </div>
                         </div>
                     </form>
